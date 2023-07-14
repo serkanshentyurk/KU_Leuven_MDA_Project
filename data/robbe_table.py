e9f9e16f680d414103e9ae0b8d18d114df224cfd
serkanshentyurk <89384346+serkanshentyurk@users.noreply.github.com>
2023-07-14 15:14:21 +0200

    data added manually
    some of them are divided due to the size issue

---
+++ data/robbe_table.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from dash import dash_table
+table_methods = pd.read_csv('/Users/Serkan/Desktop/KUL/2023 Spring/Modern Data Analytics/Project/MDABurundi/App/data/model_comparison.csv', sep='\t')
+
+dashtable_methods = dash_table.DataTable(
+        table_methods.to_dict('records'), [{"name": i, "id": i} for i in table_methods.columns],
+        style_cell={
+        'overflow': 'hidden',
+        'textOverflow': 'ellipsis',
+        'maxWidth': 0,
+        },
+        tooltip_data=[{
+            column: {'value': str(value), 'type': 'markdown'}
+            for column, value in row.items()
+        } for row in table_methods.to_dict('records')
+        ],
+        tooltip_duration=None
+    )
